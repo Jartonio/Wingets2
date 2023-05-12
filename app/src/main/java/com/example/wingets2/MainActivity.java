dad0d36e12ffee3277633f912e46867be927c42f
Antonio <antonio@asturmail.com>
2023-05-12 16:13:09 +0200

    Inicio

--- app/src/main/java/com/example/wingets2/MainActivity.java
+++ app/src/main/java/com/example/wingets2/MainActivity.java
@@ -6,39 +6,38 @@ import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    int sexo = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RadioGroup miRadioGroup = (RadioGroup) findViewById(R.id.rg_todos);
-
-        TextView display = (TextView) findViewById(R.id.display);
+        RadioGroup miRadioGroup = findViewById(R.id.rg_todos);
 
 
         miRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
 
-                RadioButton pulsado = (RadioButton) findViewById(checkedId);
+                RadioButton pulsado = findViewById(checkedId);
+                TextView display = findViewById(R.id.display);
 
-                //display.setText(pulsado.getText());
+                display.setText(pulsado.getText());
 
                 switch (pulsado.getId()) {
                     case R.id.rb_hombre:
-                        display.setText(pulsado.getText());
+                        sexo = 1;
                         break;
                     case R.id.rb_mujer:
-                        display.setText(pulsado.getText());
+                        sexo = 2;
                         break;
                     case R.id.rb_LGTBI:
-                        display.setText(pulsado.getText());
-                        break;
-                    default:
+                        sexo = 3;
                         break;
                 }
 
