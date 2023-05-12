fed4f4d14a312a8aa8ce179b424d52844b125183
Antonio <antonio@asturmail.com>
2023-05-12 15:04:23 +0200

    Inicio

--- app/src/main/java/com/example/wingets2/MainActivity.java
+++ app/src/main/java/com/example/wingets2/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this, "LGTBI", Toast.LENGTH_SHORT).show();
                         break;
 
+
                 }
 
                 }
